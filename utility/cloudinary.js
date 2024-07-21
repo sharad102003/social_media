7a0323247932f1dab4e822ea626bdc14821160e0
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-07-21 20:14:46 +0530

    Update cloudinary.js

--- utility/cloudinary.js
+++ utility/cloudinary.js
@@ -1,33 +1,28 @@
-router.post('/upload', isLoggedIn, upload.single('image'), async function(req, res) {
-  try {
-    const user = await userModel.findOne({ username: req.session.passport.user });
-    console.log('User found:', user);
+const cloudinary = require('cloudinary').v2;
 
-    let uploadUrl = null;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-    if (req.file) {
-      const buffer = req.file.buffer;
-      console.log('Buffer to upload:', buffer);
-      uploadUrl = await uploadOnCloudinary(buffer); // Upload the file buffer to Cloudinary
-      console.log('Upload URL:', uploadUrl);
-    } else {
-      console.log('No file uploaded');
-    }
-
-    // Create a new post with the Cloudinary URL
-    const post = await postModel.create({
-      picture: uploadUrl,
-      user: user._id,
-      caption: req.body.caption
+const uploadOnCloudinary = (buffer) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream({ resource_type: 'auto' }, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result.secure_url);
+      }
     });
 
-    console.log('Post created:', post);
+    if (buffer) {
+      uploadStream.end(buffer);
+    } else {
+      reject(new Error('Buffer is not provided'));
+    }
+  });
+};
 
-    user.posts.push(post._id);
-    await user.save();
-    res.redirect("/feed");
-  } catch (error) {
-    console.error('Error in /upload route:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+module.exports = uploadOnCloudinary;
+
